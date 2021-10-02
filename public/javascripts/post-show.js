a1496ccd93d212942e208aa1d486184ad63c6535
bw_flow09 <viethoang.ma1009@gmail.com>
2021-10-02 15:40:58 +0700

    add clear rating button

--- public/javascripts/post-show.js
+++ public/javascripts/post-show.js
@@ -26,4 +26,9 @@ $('.toggle-edit-form').on('click', function () {
     $(this).text() === "Edit" ? $(this).text("Cancel") : $(this).text("Edit");
     // toggle visibility of the edit review form
     $(this).siblings('.edit-review-form').toggle();
+});
+
+// Add click listener for clearing of rating from edit/new form
+$('.clear-rating').click(function () {
+    $(this).siblings('.input-no-rate').click();
 })
